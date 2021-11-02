57ab4dc22ad74e4cc4c67cd31b0cbd07a67e43af
Brunno Braga <brunnobga@gmail.com>
2021-11-02 16:40:18 -0300

    adding SPM

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+  name: "RxIGListKit",
+  platforms: [
+    .iOS(.v9),
+    .tvOS(.v9),
+  ],
+  products: [
+    .library(name: "RxIGListKit", targets: ["RxIGListKit"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+    .package(url: "https://github.com/Instagram/IGListKit.git", .branch("main")),
+  ],
+  targets: [
+    .target(
+      name: "RxIGListKit",
+      dependencies: [
+        .product(name: "IGListSwiftKit", package: "IGListKit"),
+        .product(name: "RxCocoa", package: "RxSwift"),
+      ],
+      path: "RxIGListKit/Classes"),
+  ]
+)
